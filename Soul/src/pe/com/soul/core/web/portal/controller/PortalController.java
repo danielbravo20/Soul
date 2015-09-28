da5d7117756de49e40e25b1cd603f665665de0af
LUIBRA <LUIBRA@LBRAVOF.financiero.bco>
2015-09-28 09:12:57 -0500

    no

--- Soul/src/pe/com/soul/core/web/portal/controller/PortalController.java
+++ Soul/src/pe/com/soul/core/web/portal/controller/PortalController.java
@@ -1,12 +1,8 @@
 package pe.com.soul.core.web.portal.controller;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.ejb.EJB;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,36 +11,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import pe.com.soul.core.modelo.Usuario;
-import pe.com.soul.core.seguridad.service.SeguridadServiceLocal;
-import pe.com.soul.core.service.portal.PortalServiceLocal;
 import pe.com.soul.core.web.bean.Respuesta;
+import pe.com.soul.core.web.controller.BaseController;
 
-/**
- * Servlet implementation class PortalController
- */
+
 @WebServlet("/portal/portalController")
-public class PortalController extends HttpServlet {
+public class PortalController extends BaseController {
+	
 	private static final long serialVersionUID = 1L;
-    
-	@EJB
-	SeguridadServiceLocal seguridadServiceLocal;
-	
-	@EJB
-	PortalServiceLocal portalServiceLocal;
-	
-    public PortalController() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		operacion(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		operacion(request, response);
-	}
-	
-	protected void operacion(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void operacion(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		request.setCharacterEncoding("UTF-8");
 		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").create();
@@ -55,18 +31,13 @@ public class PortalController extends HttpServlet {
 		
 		if(accion != null && session!=null){
 			
-			try { 
-				Usuario usuario = obtenerUsuario(request, session);
-				System.out.println("usuairo::: "+usuario);
+			Usuario usuario = obtenerUsuario(request, session);
+			
+			if("cu".equals(accion)){
 				respuesta.setResultado(true);
-				//respuesta.setRespuesta(usuario);
+				respuesta.setRespuesta(usuario);		
+			}else if("tp".equals(accion)){
 				
-				
-				
-			} catch (Exception e) {
-				respuesta.setResultado(false);
-				respuesta.setMensajeError(e.getMessage());
-				e.printStackTrace();
 			}
 		}
 		
@@ -75,12 +46,4 @@ public class PortalController extends HttpServlet {
 		printWriter.print(gson.toJson(respuesta));
 	}
 	
-	private Usuario obtenerUsuario(HttpServletRequest request, HttpSession session) throws Exception{
-		Usuario usuario = (Usuario)session.getAttribute(Usuario.SESSION_USUARIO_WEB_SOUL);
-		if(usuario==null){
-			usuario = seguridadServiceLocal.registrarUsuario(request.getUserPrincipal().getName(), session.getId());
-			session.setAttribute(Usuario.SESSION_USUARIO_WEB_SOUL, usuario);
-		}
-		return usuario;
-	}
 }
---
+++ SoulEJBClient/ejbModule/pe/com/soul/core/modelo/BaseBean.java
@@ -0,0 +1,7 @@
+package pe.com.soul.core.modelo;
+
+public class BaseBean implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
